3327b13eb1402183b6ad06dfd60e5461344e790d
alifnrz27 <alifnaufalrizki27@gmail.com>
2023-10-17 20:39:02 +0700

    Separate Users API for each role

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -17,10 +17,26 @@ class UserController extends Controller
         $this->middleware(['auth:api', 'admin']);
     }
 
-    public function index(){
+    public function indexAdmin(){
 
         try{
-            $users = User::select('uuid', 'role', 'name', 'username', 'email', 'mobile_number', 'gender', 'avatar')->get();
+            $users = User::select('uuid', 'role', 'name', 'username', 'email', 'mobile_number', 'gender', 'avatar')->where(['role' => 'admin'])->get();
+            return response()->json([
+                'message' => 'Success get data',
+                'users' => $users,
+            ], 200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                'message' => 'Data not found',
+            ], 404);
+        }
+    }
+
+    public function indexInstructor(){
+
+        try{
+            $users = User::select('uuid', 'role', 'name', 'username', 'email', 'mobile_number', 'gender', 'avatar')->where(['role' => 'instructor'])->get();
             return response()->json([
                 'message' => 'Success get data',
                 'users' => $users,
@@ -34,10 +50,26 @@ class UserController extends Controller
 
     }
 
-    public function store(Request $request): JsonResponse{
+    public function indexStudent(){
+
+        try{
+            $users = User::select('uuid', 'role', 'name', 'username', 'email', 'mobile_number', 'gender', 'avatar')->where(['role' => 'student'])->get();
+            return response()->json([
+                'message' => 'Success get data',
+                'users' => $users,
+            ], 200);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                'message' => 'Data not found',
+            ], 404);
+        }
+
+    }
+
+    public function storeAdmin(Request $request): JsonResponse{
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'role' => 'required|in:admin,instructor,student',
             'email' => 'required|email|unique:users',
             'username' => 'required|unique:users',
             'password' => 'required|min:8',
@@ -55,7 +87,7 @@ class UserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'username' => $request->username,
-            'role' => $request->role,
+            'role' => 'admin',
         ]);
         $user->sendEmailVerificationNotification();
         return response()->json([
@@ -63,8 +95,36 @@ class UserController extends Controller
         ], 200);
     }
 
-    public function update(Request $request, $uuid): JsonResponse{
-        $user = User::where(['uuid' => $uuid])->first();
+    public function storeInstructor(Request $request): JsonResponse{
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'username' => 'required|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $user =  User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'username' => $request->username,
+            'role' => 'instructor',
+        ]);
+        $user->sendEmailVerificationNotification();
+        return response()->json([
+            'message' => 'Success create new user'
+        ], 200);
+    }
+
+    public function updateAdmin(Request $request, $uuid): JsonResponse{
+        $user = User::where(['uuid' => $uuid, 'role' => 'admin'])->first();
 
         if(!$user){
             return response()->json([
@@ -74,7 +134,6 @@ class UserController extends Controller
 
         $validate = [
             'name' => 'required',
-            'role' => 'required|in:admin,instructor,student',
         ];
 
         if($user->email != $request->email){
@@ -96,7 +155,102 @@ class UserController extends Controller
 
         $validated = [
             'name' => $request->name,
-            'role' => $request->role,
+        ];
+
+        if($user->email != $request->email){
+            $validated['email'] = $request->email;
+        }
+
+        if($user->username != $request->username){
+            $validated['username'] = $request->username;
+        }
+
+        $user =  User::where(['uuid' => $uuid])->update($validated);
+        return response()->json([
+            'message' => 'Success update user'
+        ], 200);
+    }
+
+    public function updateInstructor(Request $request, $uuid): JsonResponse{
+        $user = User::where(['uuid' => $uuid, 'role' => 'instructor'])->first();
+
+        if(!$user){
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $validate = [
+            'name' => 'required',
+        ];
+
+        if($user->email != $request->email){
+            $validate['email'] = 'required|email|unique:users';
+        }
+
+        if($user->username != $request->username){
+            $validate['username'] = 'required|unique:users';
+        }
+
+        $validator = Validator::make($request->all(), $validate);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $validated = [
+            'name' => $request->name,
+        ];
+
+        if($user->email != $request->email){
+            $validated['email'] = $request->email;
+        }
+
+        if($user->username != $request->username){
+            $validated['username'] = $request->username;
+        }
+
+        $user =  User::where(['uuid' => $uuid])->update($validated);
+        return response()->json([
+            'message' => 'Success update user'
+        ], 200);
+    }
+
+    public function updateStudent(Request $request, $uuid): JsonResponse{
+        $user = User::where(['uuid' => $uuid, 'role' => 'student'])->first();
+
+        if(!$user){
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $validate = [
+            'name' => 'required',
+        ];
+
+        if($user->email != $request->email){
+            $validate['email'] = 'required|email|unique:users';
+        }
+
+        if($user->username != $request->username){
+            $validate['username'] = 'required|unique:users';
+        }
+
+        $validator = Validator::make($request->all(), $validate);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $validated = [
+            'name' => $request->name,
         ];
 
         if($user->email != $request->email){
--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,30 @@ Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('ver
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 Route::group(['middleware' => 'api', 'prefix' => 'v1', 'as' => 'api.',], function () {
+
     Route::post('authenticate', 'AuthController@authenticate')->name('authenticate');
     Route::post('register', 'AuthController@register')->name('register');
     Route::post('logout', 'AuthController@logout');
 
     Route::group(['middleware' => ['auth', 'admin', 'verified'], 'prefix' => 'admin', 'as' => 'admin.',], function () {
-        Route::get('users', 'Admin\UserController@index')->name('getUsers');
-        Route::post('users', 'Admin\UserController@store')->name('storeUsers');
-        Route::put('users/{uuid}', 'Admin\UserController@update')->name('updateUsers');
-        Route::delete('users/{uuid}', 'Admin\UserController@delete')->name('deleteUsers');
+        Route::group(['prefix' => 'users', 'as' => 'users.',], function () {
+            Route::group(['prefix' => 'admin', 'as' => 'admin.',], function () {
+                Route::get('', 'Admin\UserController@indexAdmin')->name('get');
+                Route::put('{uuid}', 'Admin\UserController@updateAdmin')->name('update');
+                Route::post('', 'Admin\UserController@storeAdmin')->name('store');
+            });
+            Route::group(['prefix' => 'instructor', 'as' => 'instructor.',], function () {
+                Route::get('', 'Admin\UserController@indexInstructor')->name('get');
+                Route::put('{uuid}', 'Admin\UserController@updateInstructor')->name('update');
+                Route::post('', 'Admin\UserController@storeInstructor')->name('store');
+            });
+            Route::group(['prefix' => 'student', 'as' => 'student.',], function () {
+                Route::get('', 'Admin\UserController@indexStudent')->name('get');
+                Route::put('{uuid}', 'Admin\UserController@updateStudent')->name('update');
+            });
+
+            Route::delete('users/{uuid}', 'Admin\UserController@delete')->name('delete');
+        });
+
     });
 });
