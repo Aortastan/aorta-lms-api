50a93d4718b8b6b45b22505252a92c1e881c9863
okyanawang <okyanawang@gmail.com>
2024-07-26 03:13:45 +0700

    feat: dumb

--- app/Http/Controllers/Pauli/RecordDataController.php
+++ app/Http/Controllers/Pauli/RecordDataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Pauli;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 use App\Models\PauliRecord;
 
@@ -12,8 +13,11 @@ class RecordDataController extends Controller
 {
     public function postRecord(Request $request)
     {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        $userUuid = $user->uuid;
+
         $validator = Validator::make($request->all(), [
-            'user_uuid' => 'required|exists:users,uuid',
             'selected_time' => 'required|in:1,2,5,10,15,30,60',
             'questions_attempted' => 'required|integer|min:0',
             'total_correct' => 'required|integer|min:0',
@@ -29,7 +33,7 @@ class RecordDataController extends Controller
         }
 
         $pauliRecord = PauliRecord::create([
-            'user_uuid' => $request->user_uuid,
+            'user_uuid' => $userUuid,
             'selected_time' => $request->selected_time,
             'questions_attempted' => $request->questions_attempted,
             'total_correct' => $request->total_correct,
