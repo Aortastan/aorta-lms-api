0d5a4093c2f89978fc09ec099b7c24ea8a6efcd1
Okyan Awang Ramadhana <t-okyan.ramadhana@traveloka.com>
2025-06-21 06:04:31 +0700

    Feat: Review tryout admin (#42)

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\LessonQuiz;
+use App\Models\StudentQuiz;
+use App\Models\Question;
+use App\Models\Answer;
+use App\Models\CourseLesson;
 
 class QuizController extends Controller
 {
@@ -17,7 +21,7 @@ class QuizController extends Controller
                 'description',
                 'duration',
                 'max_attempt',
-                'status',
+                'status'
             )
             ->with(['test', 'test.questions', 'test.questions.question', 'test.questions.question.answers'])
             ->where(['uuid' => $uuid])
@@ -65,6 +69,90 @@ class QuizController extends Controller
         return response()->json([
             'message' => "Success get data",
             'question' => $test,
+        ], 200);
+    }
+
+    public function reviewTryout($student_quiz_uuid, $user_uuid){
+        $student_quiz = StudentQuiz::
+        select('uuid', 'data_question', 'score', 'lesson_quiz_uuid')
+        ->where([
+            'user_uuid' => $user_uuid,
+            'uuid' => $student_quiz_uuid,
+        ])->first();
+
+        if(!$student_quiz){
+            return response()->json([
+                'message' => "Student Quiz not found",
+            ], 404);
+        }
+
+        $getQuiz = LessonQuiz::
+            select('uuid', 'title', 'lesson_uuid', 'description', 'duration', 'max_attempt')
+            ->where(['uuid' => $student_quiz->lesson_quiz_uuid])
+            ->first();
+
+        if(!$getQuiz){
+            return response()->json([
+                'message' => "Quiz not found",
+            ], 404);
+        }
+
+        $getLesson = CourseLesson::
+            where(['uuid' => $getQuiz->lesson_uuid])
+            ->first();
+
+        $data_question = json_decode($student_quiz->data_question);
+
+        $questions = [];
+        foreach ($data_question as $index => $data) {
+            $get_question = Question::where([
+                'uuid' => $data->question_uuid,
+            ])->first();
+
+            $answers = [];
+            foreach ($data->answers as $index => $answer) {
+                $get_answer = Answer::where([
+                    'uuid' => $answer->answer_uuid,
+                ])->first();
+
+                if($answer->is_correct) {
+                    $answers[] = [
+                        'answer_uuid' => $answer->answer_uuid,
+                        'is_correct' => $answer->is_correct,
+                        'correct_answer_explanation' => $get_answer->correct_answer_explanation,
+                        'is_selected' => $answer->is_selected,
+                        'answer' => $get_answer->answer,
+                        'image' => $get_answer->image,
+                    ];
+                } else {
+                    $answers[] = [
+                        'answer_uuid' => $answer->answer_uuid,
+                        'is_correct' => $answer->is_correct,
+                        'is_selected' => $answer->is_selected,
+                        'answer' => $get_answer->answer,
+                        'image' => $get_answer->image,
+                    ];
+                }
+            }
+
+            $questions[] = [
+                'question_uuid' => $get_question->uuid,
+                'question_type' => $get_question->question_type,
+                'question' => $get_question->question,
+                'file_path' => $get_question->file_path,
+                'url_path' => $get_question->url_path,
+                'file_size' => $get_question->file_size,
+                'file_duration' => $get_question->file_duration,
+                'type' => $get_question->type,
+                'hint' => $get_question->hint,
+                'answers' => $answers,
+            ];
+        }
+
+        return response()->json([
+            'message' => 'Sukses mengambil data',
+            'score' => $student_quiz->score,
+            'questions' => $questions
         ], 200);
     }
 }
