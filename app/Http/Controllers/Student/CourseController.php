54d7a6b941e9a8210d38f988cf239227e36866cf
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2025-11-03 20:30:26 +0700

    feat: update

--- app/Http/Controllers/Student/CourseController.php
+++ app/Http/Controllers/Student/CourseController.php
@@ -37,7 +37,7 @@ class CourseController extends Controller
         $filename = 'temp_watermarked_' . uniqid() . '.pdf';
         $output = storage_path("app/tmp/{$filename}");
         $course = Course::where('uuid', $coursUuid)->first();
-        $text = $user->name . "\n" . $user->username . "\n" . $user->email . "\n" . $user->mobile_number . "\n" . $course->title;
+        $fullText = $user->name . "\n" . $user->username . "\n" . $user->email . "\n" . $user->mobile_number . "\n" . $course->title;
 
         // Ensure temp directory exists
         if (!is_dir(storage_path('app/tmp'))) {
@@ -66,7 +66,7 @@ class CourseController extends Controller
             // Rotate and print watermark text
             $pdf->StartTransform();
             $pdf->Rotate(45, $size['width'] / 2, $size['height'] / 2);
-            $pdf->MultiCell(0, 4, $text);
+            $pdf->MultiCell($size['width'] / 2 - 20, $size['height'] / 2, $fullText);
             $pdf->StopTransform();
 
             // Reset transparency
