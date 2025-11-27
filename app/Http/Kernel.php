3619d4726a44a1aaf90729c27dd115f3347f53ea
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2025-11-28 03:18:55 +0700

    feat: temp remove throttle

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:500,1',
+            // 'throttle:500,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
