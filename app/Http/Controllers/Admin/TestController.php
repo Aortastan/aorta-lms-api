f4a40f95edaba146428e97ac352f6d49ef678e9a
nanmax <nandarifaturohman6@gmail.com>
2026-06-03 17:40:06 +0700

    add pembahasan essay - preview test

--- app/Http/Controllers/Admin/TestController.php
+++ app/Http/Controllers/Admin/TestController.php
@@ -201,6 +201,7 @@ class TestController extends Controller
                 'url_path' => $data['question']['url_path'],
                 'type' => $data['question']['type'],
                 'hint' => $data['question']['hint'],
+                'discussion' => $data['question']['discussion'],
                 'answers' => $answers,
             ];
         }
